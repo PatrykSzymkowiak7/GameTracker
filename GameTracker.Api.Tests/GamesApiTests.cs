482a015b3868d15ba7d305b24f400deac51d6ee5
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2026-08-22 13:23:21 +0200

    API Integration tests

---
+++ GameTracker.Api.Tests/GamesApiTests.cs
@@ -0,0 +1,209 @@
+﻿using GameTracker.Api.Tests.Infrastructure;
+using GameTracker.Application.DTOs;
+using GameTracker.Domain.Enums;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace GameTracker.Api.Tests
+{
+    public class GamesApiTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly CustomWebApplicationFactory _factory;
+
+        public GamesApiTests(CustomWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetGames_ReturnsSuccessStatusCode()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/api/Games");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        private async Task GetGames_ReturnsSeededGames()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("api/Games");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content
+                .ReadFromJsonAsync<PagedResultDto<GameDto>>();
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.TotalCount);
+            Assert.Equal(2, result.Items.Count());
+        }
+
+        [Fact]
+        public async Task CreateGame_WithValidData_ReturnsCreated()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var request = new CreateGameDto
+            {
+                Title = "Cyberpunk 2077",
+                Genre = "RPG",
+                Platform = Platform.PC,
+                Status = GameStatus.Backlog,
+                Rating = 9,
+                HoursPlayed = 0
+            };
+
+            var response = await client.PostAsJsonAsync(
+                "api/Games",
+                request);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            var createdGame = await response.Content
+                .ReadFromJsonAsync<GameDto>();
+
+            Assert.NotNull(createdGame);
+            Assert.Equal(request.Title, createdGame.Title);
+        }
+
+        [Fact]
+        public async Task CreateGame_WithExistingTItle_ReturnsConflict()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var request = new CreateGameDto
+            {
+                Title = "Elden Ring",
+                Genre = "RPG",
+                Platform = Platform.PC,
+                Status = GameStatus.Backlog
+            };
+
+            var response = await client.PostAsJsonAsync(
+                "api/Games",
+                request);
+
+            Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteGame_WithId_ReturnsNoContent()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var response = await client.DeleteAsync("api/Games/1");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+            response = await client.GetAsync("api/Games/1");
+
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateGame_WithValidData_ReturnsUpdatedGame()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var request = new UpdateGameDto
+            {
+                Title = "Elden Ring",
+                Genre = "RPG",
+                Platform = Platform.PC,
+                Status = GameStatus.Completed,
+                Rating = 10,
+                HoursPlayed = 150
+            };
+
+            var response = await client.PutAsJsonAsync(
+                "api/Games/1",
+                request);
+
+            var updatedGame = await response.Content
+                .ReadFromJsonAsync<GameDto>();
+
+            Assert.NotNull(updatedGame);
+            Assert.Equal(GameStatus.Completed, updatedGame.Status);
+            Assert.Equal(150, updatedGame.HoursPlayed);
+        }
+
+        [Fact]
+        public async Task UpdateGame_WhenGameDoesNotExist_ReturnsNotFound()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var request = new UpdateGameDto
+            {
+                Title = "Unknown Game",
+                Genre = "RPG",
+                Platform = Platform.PC,
+                Status = GameStatus.Backlog,
+                Rating = 5,
+                HoursPlayed = 0
+            };
+
+            var response = await client.PutAsJsonAsync(
+                "api/Games/999",
+                request);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteGame_WHenGameDoesNotExist_ReturnsNotFound()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var response = await client.DeleteAsync("api/Games/999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateGame_WithInvalidData_ReturnsBadRequest()
+        {
+            await _factory.ResetDatabaseAsync();
+
+            var client = _factory.CreateClient();
+
+            var request = new CreateGameDto
+            {
+                Title = "",
+                Genre = "RPG",
+                Platform = Platform.PC,
+                Status = GameStatus.Backlog,
+                Rating = 15,
+                HoursPlayed = -10
+            };
+
+            var response = await client.PostAsJsonAsync(
+                "/api/Games",
+                request);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
